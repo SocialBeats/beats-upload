ef143a944dca1db7232843865eb1d85a21db3b47
MiguelEncina <migencmar@alum.us.es>
2025-11-19 12:28:16 +0100

    feat: Beat entity initial version

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+// Exportar todos los modelos desde un solo lugar
+export { default as Beat } from './Beat.js';
+
+// Cuando agregues más modelos, los exportas aquí:
+// export { default as User } from './User.js';
+// export { default as Playlist } from './Playlist.js';
