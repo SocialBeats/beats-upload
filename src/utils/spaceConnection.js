4119d8116845ae7b0bd9ee26a86d56dcc1534c62
MiguelEncina <migencmar@alum.us.es>
2026-01-05 02:17:55 +0100

    fix: trying to solve action test

--- src/utils/spaceConnection.js
+++ src/utils/spaceConnection.js
@@ -2,5 +2,5 @@ import { connect } from 'space-node-client';
 
 export const spaceClient = connect({
   url: process.env.SPACE_URL || 'http://localhost:5403/',
-  apiKey: process.env.SPACE_API_KEY,
+  apiKey: process.env.SPACE_API_KEY || '',
 });
